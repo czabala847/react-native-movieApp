6cf171433dd672117c352703b02d11238bcf48c2
Carlos Zabala <czabala847@gmail.com>
2022-12-16 23:34:08 -0500

    Implementando carrusel para los poster de las peliculas

---
+++ src/components/MoviePoster.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View, Image, StyleSheet} from 'react-native';
+import {Movie} from '../interfaces/movieInterface';
+
+interface Props {
+  movie: Movie;
+}
+
+export const MoviePoster: React.FC<Props> = ({movie}) => {
+  const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
+  return (
+    <View
+      style={{
+        height: 420,
+        width: 300,
+      }}>
+      <View style={styles.imageContainer}>
+        <Image source={{uri}} style={styles.image} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    borderRadius: 18,
+    elevation: 5,
+    flex: 1,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  image: {
+    borderRadius: 18,
+    flex: 1,
+    shadowColor: '#000',
+  },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,17 @@
-import {StackScreenProps} from '@react-navigation/stack/lib/typescript/src/types';
+// import {StackScreenProps} from '@react-navigation/stack/lib/typescript/src/types';
 import React from 'react';
-import {ActivityIndicator, Button, Text, View} from 'react-native';
+import {ActivityIndicator, Dimensions, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Carousel from 'react-native-snap-carousel';
+import {MoviePoster} from '../components/MoviePoster';
 import {useMovies} from '../hooks/useMovies';
 
-interface Props extends StackScreenProps<any, any> {}
+// interface Props extends StackScreenProps<any, any> {}
 
-export const HomeScreen = ({navigation}: Props) => {
+const {width: windowWidth} = Dimensions.get('window');
+
+export const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
   const {isLoading, moviesNowPlaying} = useMovies();
 
   if (isLoading) {
@@ -22,12 +28,21 @@ export const HomeScreen = ({navigation}: Props) => {
   }
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Button
-        title="Ir al detalle"
-        onPress={() => navigation.navigate('DetailScreen')}
-      />
+    <View
+      style={{
+        marginTop: top + 20,
+      }}>
+      <View
+        style={{
+          height: 440,
+        }}>
+        <Carousel
+          data={moviesNowPlaying}
+          renderItem={({item}) => <MoviePoster movie={item} />}
+          sliderWidth={windowWidth}
+          itemWidth={300}
+        />
+      </View>
     </View>
   );
 };
